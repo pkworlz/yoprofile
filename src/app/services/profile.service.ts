6225964d0e51950e7e3020979888367dd4ff2415
Prem Kumar <pkworlz@gmail.com>
2020-11-21 18:46:17 +0530

    local storage attached..

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  profiles : Profile [] = [
-    {
-      id : 1,
-      name : "Prem Kumar",
-      email : "pkworlz@gmail.com",
-    }
-  ];
+  profiles : Profile [] = [];
 
-  constructor() { }
+  constructor(
+    private storage: Storage
+  ) { 
+    this.getFromLocal();
+  }
+
+
 
   get() {
     return this.profiles;
@@ -21,13 +22,31 @@ export class ProfileService {
 
   add(profile:Profile){
     this.profiles.push(profile);
+    this.saveToLocal();
   }
 
   remove(profile: Profile) {
     let index = this.profiles.indexOf(profile);
     this.profiles.splice(index,1);
     console.log(this.profiles);
-    
+    this.saveToLocal();
+  }
+
+
+  // persistent features..
+  private saveToLocal() {
+    this.storage.set("ProfileKey", this.profiles);
+  }
+
+  private getFromLocal() {
+    this.storage.get("ProfileKey").then((localProfiles) => {
+      console.log(localProfiles);
+      if (localProfiles === null) {
+        this.profiles = [];
+      } else {
+        this.profiles = localProfiles;
+      }
+    });;
   }
 
 }
