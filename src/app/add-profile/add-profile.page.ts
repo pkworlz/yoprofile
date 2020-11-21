c2a6d7f03fb8f2531323ade2454c0bc11dce900d
Prem Kumar <pkworlz@gmail.com>
2020-11-21 19:21:55 +0530

    Camera feature init..

--- src/app/add-profile/add-profile.page.ts
+++ src/app/add-profile/add-profile.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
-import { profile } from 'console';
 import { Profile, ProfileService } from '../services/profile.service';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-add-profile',
@@ -17,15 +17,38 @@ export class AddProfilePage implements OnInit {
     imageUrl: ""
   };
 
+  options: CameraOptions = {
+    quality: 100,
+    destinationType: this.camera.DestinationType.FILE_URI,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE
+  }
+
   constructor(
     private profileService : ProfileService,
     private navCtrl: NavController,
+    private camera: Camera,
 
   ) { }
 
 
   ngOnInit() {
     this.newProfile.imageUrl = "https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y";
+  }
+
+  takePicture() {
+
+    this.camera.getPicture(this.options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64 (DATA_URL):
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+      console.log(imageData);
+      
+     }, (err) => {
+      // Handle error
+      console.log(err);
+      
+     });
   }
 
   submitProfile() {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { IonicStorageModule } from '@ionic/storage';
+import { Camera } from '@ionic-native/camera/ngx';
 
 @NgModule({
   declarations: [AppComponent],
@@ -17,6 +18,7 @@ import { IonicStorageModule } from '@ionic/storage';
   providers: [
     StatusBar,
     SplashScreen,
+    Camera,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
